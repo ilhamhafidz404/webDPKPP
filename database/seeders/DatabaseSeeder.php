cb91f3d62081e588bd498e7d08a38c2355069857
ilhamhafidz404 <xxspanzie@gmail.com>
2021-10-05 09:11:28 +0700

    User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\InventorySeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     // \App\Models\User::factory(10)->create();
 
     $this->call(InventorySeeder::class);
+    $this->call(UserSeeder::class);
   }
 }
